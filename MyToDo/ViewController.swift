4a10129d4854f733b858972b1f617bda0b04d1cd
assemsemsem <assemsarsenbay08@gmail.com>
2021-01-09 22:08:38 +0500

    Initial Commit

---
+++ MyToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyToDo
+//
+//  Created by Assem on 1/9/21.
+//  Copyright © 2021 Assem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
